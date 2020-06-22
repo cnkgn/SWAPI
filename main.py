ec11258e71a92383f33a2ea1a15c287961126aae
A <aakgun1@gmail.com>
2020-06-22 15:18:22 +0300

    People Added

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello World":"From Can"}
+    return {"Hello":"SWAPI"}
+
+people = ["Luke Skywalker", "Chewbacca", "Yoda", "Jedi", "Storm Trooper"]
+
+@app.get("/people/{person_id}")
+def read_person(person_id : int):
+    str = people[person_id - 1]
+    return {"This is":str}
